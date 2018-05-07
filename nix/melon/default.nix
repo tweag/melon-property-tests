26680f3793659411ee9119e15ac58da97642b5fc
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-05-07 19:25:15 +0200

    add melon dependencies

--- nix/melon/default.nix
+++ nix/melon/default.nix
@@ -1,6 +1,6 @@
-{ mkDerivation, aeson, aeson-casing, base, bytestring, data-default
-, hedgehog, lens, lens-aeson, path, path-io, process
-, safe-exceptions, stdenv, web3
+{ mkDerivation, aeson, aeson-casing, aeson-pretty, base, bytestring
+, data-default, hedgehog, lens, lens-aeson, path, path-io, process
+, safe-exceptions, stdenv, unordered-containers, vector, web3
 }:
 mkDerivation {
   pname = "melon";
@@ -13,7 +13,8 @@ mkDerivation {
     safe-exceptions web3
   ];
   executableHaskellDepends = [
-    base data-default hedgehog path path-io web3
+    aeson aeson-pretty base bytestring data-default hedgehog lens
+    lens-aeson path path-io unordered-containers vector web3
   ];
   description = "Property based random tests for Melon smart-contract";
   license = stdenv.lib.licenses.gpl3;
