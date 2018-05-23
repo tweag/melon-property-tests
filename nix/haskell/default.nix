b476becba5e7a9330863466b71aebe7a60eb76b9
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-05-23 12:14:40 +0200

    Exclude hs-web3 itself from Nix shell
    Since the package is a git submodule we only want to include its
    dependencies but not the package itself in the Nix shell.

--- nix/haskell/default.nix
+++ nix/haskell/default.nix
@@ -2,5 +2,5 @@
 # Build a new overlay with our own packages
 haskell.packages.ghc822.extend (self: super: {
   melon = super.callPackage ../melon {};
-  web3 = haskell.lib.dontCheck (super.callPackage ../hs-web3 {});
+  web3 = super.callPackage ../hs-web3 {};
 })
--- shell.nix
+++ shell.nix
@@ -4,11 +4,51 @@ in
 
 with pkgs;
 
+let
+  ghcWithDependenciesAndSystemDependencies =
+    let
+      internalHaskellPackages = [ "melon" "web3" ];
+      haskellPackages = pkgs.haskellPackages;
+      # Check that a Haskell package is external to this repository.
+      isExternalPackage = x:
+        lib.all (internalName: x.pname or "" != internalName)
+        internalHaskellPackages
+      ;
+      # Determine if a package is a Haskell package or not.  Stolen from:
+      # <nixpkgs/pkgs/development/haskell-modules/generic-builder.nix>
+      isHaskellPkg = x: (x ? pname) && (x ? version) && (x ? env);
+      isSystemPkg = x: !isHaskellPkg x;
+
+      allDependencies =
+        lib.concatMap
+          ( pname:
+            let pkg = haskellPackages."${pname}"; in
+            lib.concatLists [
+              pkg.buildInputs
+              pkg.nativeBuildInputs
+              pkg.propagatedBuildInputs
+              pkg.propagatedNativeBuildInputs
+            ]
+          )
+          internalHaskellPackages
+      ;
+      haskellDependencies =
+        builtins.filter
+          (x: isHaskellPkg x && isExternalPackage x)
+          allDependencies
+      ;
+      systemDependencies = builtins.filter isSystemPkg allDependencies;
+      ghcWithDependencies = haskellPackages.ghcWithPackages
+        (ps: with ps; haskellDependencies)
+      ;
+    in
+    { inherit ghcWithDependencies systemDependencies; }
+  ;
+in with ghcWithDependenciesAndSystemDependencies;
+
 mkShell {
   inputsFrom = [
     smart-contracts.shell
-    haskellPackages.web3.env
-    haskellPackages.melon.env
   ];
 
   nativeBuildInputs = [
@@ -24,6 +64,10 @@ mkShell {
   ];
 
   buildInputs = [
+    ghcWithDependencies
+    systemDependencies
     zlib
   ];
+
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
 }
