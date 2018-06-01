7f829517462cc8b683896ffbcd4276186ca7c348
Andreas Herrmann <andreas.herrmann@tweag.io>
2018-06-01 14:29:33 +0200

    Fix node version. It should be node 8

--- nix/all-packages.nix
+++ nix/all-packages.nix
@@ -11,7 +11,18 @@ with super;
 
   haskellPackages = callPackage ./haskell {};
 
-  smart-contracts = callPackage ./smart-contracts {};
+  smart-contracts =
+    let
+      smart-contracts = callPackage ./smart-contracts {
+        nodejs = self.nodejs-8_x;
+      };
+    in
+    smart-contracts // {
+      package = smart-contracts.package.overrideAttrs (attrs: {
+        nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [ self.git ];
+      });
+    }
+  ;
 
   scripts = {
     update-node-deps = callPackage ./scripts/update-node-deps.nix {};
